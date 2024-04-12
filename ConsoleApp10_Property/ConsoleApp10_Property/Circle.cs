8232d812cf76c16fe6e39c4fa121d3b7915d1c99
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-04-12 15:16:17 +0530

    Property in C#

---
+++ ConsoleApp10_Property/ConsoleApp10_Property/Circle.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*The property name must be same as the feild name but to avoid naming colision  use '_' as prefix with feild.*/
+namespace ConsoleApp10_Property
+{
+    public class Circle
+    {
+        //by default all the fields are private
+        double _Radius = 12.35;
+
+        /*
+        //Set Access 
+        public void SetRadius(double r)
+        {
+            this._Radius = r;
+        }
+
+        //Get Access
+        public double GetRadius()
+        {
+            return _Radius;
+        }
+        
+
+        /*
+         * In above code you gave set and get access to the private radius 
+         * but you can do the same thing by using public variable but you can not restrict the value.
+         * 
+         * so, to do so  you can define a property - instead of writing 2 methods you can achive the same
+         * result by defining only 1 property
+         */
+
+
+        //property - property doesn't have braces () , it looks like a simple feild
+        public double Radius
+        {
+            get { return _Radius; }
+            // set { _Radius = value; }
+
+            //you can also do conditional access and conditional assignment
+            set
+            {
+                if(value > _Radius)
+                {
+                    _Radius = value;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp10_Property/ConsoleApp10_Property/TestCircle.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/* Property 
+ * It is combination of 2 methods
+ */
+namespace ConsoleApp10_Property
+{
+    internal class TestCircle
+    {
+        static void Main(string[] args)
+        {
+            Circle c = new Circle();
+            // Console.WriteLine(c.Radius); -- bz. of private you can not access it directly
+
+            double Ans = c.Radius;
+            Console.WriteLine(Ans);
+
+            c.Radius = 12.33;
+            Console.WriteLine(c.Radius);
+
+            Console.ReadLine();
+        }
+    }
+}
