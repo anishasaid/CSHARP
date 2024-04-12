99e90f8cebbb7c5cdd2ab2b93ce9035cf8757cc5
anishasaid <116268672+anishasaid@users.noreply.github.com>
2024-04-12 15:15:28 +0530

    Structure in C#

---
+++ ConsoleApp8_Structure/ConsoleApp8_Structure/MyStruct.cs
@@ -0,0 +1,48 @@
+﻿using System;
+// Structure in CSharp
+/*
+ * 1. User Defined Data Type
+ * 2. Syntax is same as that of class.
+ * 3. Used for smaller data entities.
+ * 4. Creating obejects with new keyword is optional 
+ * 5. if you create 0 constructors in structure then after compilation their will be 1 constructor (implicit)
+ *    and if you define "n" costructors in it then after compilation their will be n+1 constructors.
+ *    Because you can not explicitly define parameterless constructor if you did you will get an error
+ *    
+ */
+struct MyStruct
+{
+    //int i = 10;  -- you can not initialized field at the time of decalration
+    int i;
+
+    //parameterised constructor
+    public MyStruct(int i)
+    {
+        this.i = i;
+    }
+
+    public void Display()
+    {
+        Console.WriteLine("Display method of a structor" + i);
+    }
+
+    static void Main(string[] args)
+    {
+        //Follwing are differernt ways to initialize the instance variable i
+
+        //1. by using default constructor 
+        MyStruct M = new MyStruct();
+        M.Display();
+
+        //2. by not using new keyword 
+        MyStruct M1;
+        M1.i = 10;
+        M1.Display();
+
+        //3. by using parameterised constructor
+        MyStruct M3 = new MyStruct(11);
+        M3.Display();
+
+        Console.ReadLine();
+    }
+}
---
+++ ConsoleApp8_Structure/ConsoleApp8_Structure/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 
+namespace ConsoleApp8_Structure
+{
+    internal class Program
+    {
+        //in class you can initialized fields at the time of decalration
+        int i = 55;
+
+        static void Main(string[] args)
+        {
+            // in a class to access the feilds the new keyword is mandatory where it is optional in c#
+            Program p = new Program();
+            Console.WriteLine(p.i);
+        }
+    }
+}
